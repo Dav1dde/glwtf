9673157de6054188fd2030e6abf9af6401dfe93f
David <admin@dav1d.de>
2013-01-17 19:07:19 +0100

    added properties to window

--- glwtf/window.d
+++ glwtf/window.d
@@ -9,6 +9,7 @@ private {
 
     import std.string : toStringz;
     import std.exception : enforceEx;
+    import std.functional : curry;
 }
 
 
@@ -17,8 +18,35 @@ struct Rect {
     int y;
 }
 
+private string set_hint_property(string target, string name, bool getter=false) {
+    string ret = `@property void ` ~ name ~ `(int hint) {
+                      set_hint(` ~ target ~ `, hint);
+                  }`;
+
+    if(getter) {
+        ret ~=   `@property int ` ~ name ~ `() {
+                      return get_param(` ~ target ~ `);
+                  }`;
+    }
+
+    return ret;
+}
+
+
 class Window : BaseGLFWEventHandler {
-    void* window;
+    debug {
+        private void* _window;
+
+        @property void* window() {
+            assert(_window !is null, "no window created yet!");
+            return _window;
+        }
+        @property void window(void* window) {
+            _window = window;
+        }
+    } else {
+        void* window;
+    }
     protected DefaultAA!(bool, int, false) keymap;
     protected DefaultAA!(bool, int, false) mousemap;
 
@@ -37,6 +65,33 @@ class Window : BaseGLFWEventHandler {
     void set_hint(int target, int hint) {
         glfwWindowHint(target, hint);
     }
+
+    mixin(set_hint_property("GLFW_RED_BITS", "red_bits"));
+    mixin(set_hint_property("GLFW_GREEN_BITS", "green_bits"));
+    mixin(set_hint_property("GLFW_BLUE_BITS", "blue_bits"));
+    mixin(set_hint_property("GLFW_ALPHA_BITS", "alpha_bits"));
+    mixin(set_hint_property("GLFW_DEPTH_BITS", "depth_bits"));
+    mixin(set_hint_property("GLFW_STENCIL_BITS", "stencil_bits"));
+    mixin(set_hint_property("GLFW_ACCUM_RED_BITS", "accum_red_bits"));
+    mixin(set_hint_property("GLFW_ACCUM_GREEN_BITS", "accum_green_bits"));
+    mixin(set_hint_property("GLFW_ACCUM_BLUE_BITS", "accum_blue_bits"));
+    mixin(set_hint_property("GLFW_ACCUM_ALPHA_BITS", "accum_alpha_bits"));
+    mixin(set_hint_property("GLFW_AUX_BUFFERS", "aux_buffers"));
+    mixin(set_hint_property("GLFW_STEREO", "stereo"));
+    mixin(set_hint_property("GLFW_SAMPLES", "samples"));
+    mixin(set_hint_property("GLFW_SRGB_CAPABLE", "srgb_capable"));
+    mixin(set_hint_property("GLFW_CLIENT_API", "client_api", true));
+    mixin(set_hint_property("GLFW_OPENGL_API", "opengl_api"));
+    mixin(set_hint_property("GLFW_CONTEXT_VERSION_MAJOR", "context_version_major", true));
+    mixin(set_hint_property("GLFW_CONTEXT_VERSION_MINOR", "context_version_minor", true));
+    mixin(set_hint_property("GLFW_OPENGL_FORWARD_COMPAT", "opengl_forward_compat", true));
+    mixin(set_hint_property("GLFW_OPENGL_DEBUG_CONTEXT", "opengl_debug_context", true));
+    mixin(set_hint_property("GLFW_OPENGL_PROFILE", "opengl_profile", true));
+    mixin(set_hint_property("GLFW_CONTEXT_ROBUSTNESS", "context_robustness", true));
+    mixin(set_hint_property("GLFW_RESIZABLE", "resizable", true));
+    mixin(set_hint_property("GLFW_VISIBLE", "visible", true));
+    mixin(set_hint_property("GLFW_POSITION_X", "position_x", true));
+    mixin(set_hint_property("GLFW_POSITION_Y", "position_y", true));
 
     void create(int width, int height, string title, void* monitor = null, void* share = null) {
         window = glfwCreateWindow(width, height, title.toStringz(), monitor, share);
